dbb796a8a090eb08d2d315920964af91faa0de4c
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-09-13 21:08:47 +0400

    finish 3.8*: Phonebook backend step8, morgan shows data sent in HTTP POST request

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.use(express.json());
 
 app.use(requestLogger)*/
 
-app.use(morgan('tiny'))
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
 
 //Data
 let persons = [
