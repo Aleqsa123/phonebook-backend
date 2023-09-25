94c0963187d13d82bd3725f7bda3b9e3ba641160
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-09-25 22:51:52 +0400

    modify index.js, add express.static(build)

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use(express.static('build'))
+
 //Morgan API request logger
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
